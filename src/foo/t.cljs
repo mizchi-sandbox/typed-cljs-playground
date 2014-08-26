d2ed09a253743979e1df531b69fb88901d78242c
Koutarou Chikuba <miz404@gmail.com>
2014-08-27 03:12:17 +0900

    tried

--- src/foo/t.cljs
+++ src/foo/t.cljs
@@ -1,5 +1,21 @@
 (ns foo.t
-  (:require [cljs.core.typed :refer-macros [ann check-ns]]))
+  (:require [cljs.core.typed :as t])
+  (:require-macros [cljs.core.typed :as t :refer [ann]]))
 
-(ann foo number)
+(ann foo t/Number)
 (def foo 1)
+
+(ann bar [t/Number -> t/Number])
+(defn bar [n] n)
+
+(ann baz [t/Number t/String -> t/String])
+(defn baz [n str] str)
+
+; (ann x-point [{:x t/Number :y t/Number} -> t/Number])
+; (defn x-point [p] 1)
+
+; (ann-form #(+ 1 %) [t/Number -> t/Number])
+; (ann-form (fn [n] n) [t/Number -> t/Number])
+
+; (ann id (t/U -> t/U))
+; (defn id [x] x)
